13683ee17adfbbe9e22df826ec2aaa8e045728c2
Kallol Borah <kallol.borah@gmail.com>
2021-01-18 10:18:28 +0530

    format function params for user registration and kyc

--- wallet/register.js
+++ wallet/register.js
@@ -10,54 +10,73 @@ const vkyc = artifacts.require('VerifiedKYC');
 const ClientAddress = process.env.CLIENT;
 const KYCAddress = process.env.KYC;
 
-async function setKycStatus(client, status){
+async function setKycStatus(_client, _status){
     const KYC = new ethers.Contract(KYCAddress, vkyc, signer);
-    await KYC.setStatus(client, status);
+    client = $("#_client").val();
+    status = $("#_status").val();
+    await KYC.setStatus(ethers.utils.getAddress(client), ethers.utils.formatBytes32String(status));
 }
 
-async function getKycStatus(callback, client){
+async function getKycStatus(callback, _client){
     const KYC = new ethers.Contract(KYCAddress, vkyc, provider);
-    callback(await KYC.getStatus(client));
+    client = $("#_client").val();
+    callback(await KYC.getStatus(ethers.utils.getAddress(client)));
 }
 
-async function updateKycRecord(client, file, address, photoId, videoId, fatca, crs){
+async function updateKycRecord(_client, _file, _address, _photoId, _videoId, _fatca, _crs){
     const KYC = new ethers.Contract(KYCAddress, vkyc, signer);
-    await KYC.setFile(client, file);
-    await KYC.setAddress(client, address);
-    await KYC.setPhotoID(client, photoId);
-    await KYC.setVideoID(client, videoId);
-    await KYC.setFATCA(client, fatca);
-    await KYC.setCRS(client, crs);
+    client = $("#_client").val();
+    file = $("#_file").val();
+    address = $("#_address").val();
+    photoId = $("#_photoId").val();
+    videoId = $("#_videoId").val();
+    fatca = $("#_fatca").val();
+    crs = $("#_crs").val();
+    await KYC.setFile(ethers.utils.getAddress(client), ethers.utils.formatBytes32String(file));
+    await KYC.setAddress(ethers.utils.getAddress(client), ethers.utils.formatBytes32String(address));
+    await KYC.setPhotoID(ethers.utils.getAddress(client), ethers.utils.formatBytes32String(photoId));
+    await KYC.setVideoID(ethers.utils.getAddress(client), ethers.utils.formatBytes32String(videoId));
+    await KYC.setFATCA(ethers.utils.getAddress(client), ethers.utils.formatBytes32String(fatca));
+    await KYC.setCRS(ethers.utils.getAddress(client), ethers.utils.formatBytes32String(crs));
 }
 
-async function setCustodian(client, custodian){
+async function setCustodian(_client, _custodian){
     const Client = new ethers.Contract(ClientAddress, vclient, signer);
-    await Client.setCustody(client, custodian);
+    client = $("#_client").val();
+    custodian = $("#_custodian").val();
+    await Client.setCustody(ethers.utils.getAddress(client), ethers.utils.getAddress(custodian));
 }
 
-async function getCustodian(callback, client){
+async function getCustodian(callback, _client){
     const Client = new ethers.Contract(ClientAddress, vclient, provider);
-    callback(await Client.getCustody(client));
+    client = $("#_client").val();
+    callback(await Client.getCustody(ethers.utils.getAddress(client)));
 }
 
-async function setAccess(login){
+async function setAccess(_login){
     const Client = new ethers.Contract(ClientAddress, vclient, signer);
-    await Client.setAccess(login);
+    login = $("#_login").val();
+    await Client.setAccess(ethers.utils.formatBytes32String(login));
 }
 
-async function getAccess(callback, client){
+async function getAccess(callback, _client){
     const Client = new ethers.Contract(ClientAddress, vclient, provider);
-    callback(await Client.getAccess(client));
+    client = $("#_client").val();
+    callback(await Client.getAccess(ethers.utils.getAddress(client)));
 }
 
-async function setManager(client, manager){
+async function setManager(_client, _manager){
     const Client = new ethers.Contract(ClientAddress, vclient, signer);
-    await Client.setManager(client, manager);
+    client = $("#_client").val();
+    manager = $("#_manager").val();
+    await Client.setManager(ethers.utils.getAddress(client), ethers.utils.getAddress(manager));
 }
 
-async function getManager(callback, client, requestor){
+async function getManager(callback, _client, _requestor){
     const Client = new ethers.Contract(ClientAddress, vclient, provider);
-    callback(await Client.getManager(client, requestor));
+    client = $("#_client").val();
+    requestor = $("#_requestor").val();
+    callback(await Client.getManager(ethers.utils.getAddress(client), ethers.utils.getAddress(requestor)));
 }
 
 // create wallet
