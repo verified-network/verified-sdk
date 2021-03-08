dde6b06d99a48a2d72e48c059b4da0dabc572614
gmustafa <gmustafa.mansoori@gmail.com>
2021-03-08 19:05:24 +0530

    config working for import , not working for require

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,4 @@
-import {ethers} from 'ethers';
-
-function createWallet(): any {
-    let wallet = ethers.Wallet.createRandom();
-    return {
-        privateKey: wallet.privateKey,
-        address: wallet.address,
-        publicKey: wallet.publicKey,
-        mnemonic: wallet.mnemonic
-    }
-}
-
-function recoverWallet(mnemonic?: string): any {
-    if (mnemonic !== undefined) {
-        return ethers.Wallet.fromMnemonic(mnemonic);
-    } else {
-        return new Error('mnemonic undefined');
-    }
-}
-
-export { createWallet, recoverWallet}
-
-
-
-
-
-
+export { createWallet, recoverWallet} from './wallet/wallet';
 
 
 
