bff810c9ad1542dea20fddfa0b0d1a2f46cbcec7
Achin <achin.k@rejolut.com>
2021-03-22 20:26:58 +0530

    added comment to the functions

--- src/contract/client/index.ts
+++ src/contract/client/index.ts
@@ -40,7 +40,14 @@ export default class ClientContract extends VerifiedContract {
         return this.callContract(FUNCTIONS.GETCUSTODY, params)
     }
 
-    // function mentioned in the document to be integrated
+    /**
+     * We can implement registration and log in using a SSO scheme such as Firebase (https://firebase.google.com/docs/auth) or Azure. 
+     * We host our infra on Azure, so that might be preferable. This will enable google / facebook / twitter / Microsoft users to log in to our application. 
+     * The SSO system will provide us with a token. Once logged in, the Verified Dapp should call the following solidity function. 
+     * This should only be called after the user’s KYC is complete.
+     * @param {bool login} 
+     * @returns 
+     */
     public setAccess(params: SetAccess): any {
         return this.callContract(FUNCTIONS.SETACCESS, params)
     }
@@ -49,7 +56,13 @@ export default class ClientContract extends VerifiedContract {
         return this.callContract(FUNCTIONS.GETACCESS, params)
     }
 
-    // function mentioned in the document to be integrated
+    /**
+     * Once a user’s wallet is set up, the Dapp should register the user for KYC (know your customer) process
+     *  with the issuer. By default, Verified itself is the issuer. In the future, we may have country specific
+     *  issuers. This is done by calling the following solidity contract function
+     * @param {address _client, address _manager} 
+     * @returns 
+     */
     public setManager(params: SetManager): any {
         return this.callContract(FUNCTIONS.SETMANAGER, params)
     }
@@ -63,6 +76,13 @@ export default class ClientContract extends VerifiedContract {
         return this.callContract(FUNCTIONS.ISREGISTERED, params)
     }
 
+
+    /**
+     * We are going to use Coinfirm’s anti-money laundering score (cscore in the json response) to decide whether to block a user or not.
+     * Any cscore below 33 needs to be flagged to status equal to false in the following solidity function call.
+     * @param {(address _client, bool status} 
+     * @returns 
+     */
     public setAMLStatus(params: SetAMLStatus): any {
         return this.callContract(FUNCTIONS.SETAMLSTATUS, params)
     }
@@ -71,7 +91,12 @@ export default class ClientContract extends VerifiedContract {
         return this.callContract(FUNCTIONS.GETAMLSTATUS, params)
     }
 
-    //
+    /**
+     * The following solidity function should be called passing the issuer’s address as parameter where,
+     *  _status equal to false will fetch all investors whose KYC is not yet complete.
+     * @param {address _manager,bool _status} 
+     * @returns {address[] memory}
+     */
     public getClients(params: GetClients): any {
         return this.callContract(FUNCTIONS.GETCLIENTS, params)
     }
