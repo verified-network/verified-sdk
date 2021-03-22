d1bd28afa0548f5fb9e6bcb674298df33211e62a
Achin <achin.k@rejolut.com>
2021-03-22 20:30:39 +0530

    added documentation ledger

--- src/contract/ledger/index.ts
+++ src/contract/ledger/index.ts
@@ -10,7 +10,7 @@ enum FUNCTIONS {
     CREATEACCOUNT = 'createAccount'
 }
 
-export default class ClientContract extends VerifiedContract {
+export default class LedgerContract extends VerifiedContract {
 
     constructor(signer: VerifiedWallet) {
 
@@ -18,6 +18,13 @@ export default class ClientContract extends VerifiedContract {
         super(contractAddress[network].KYC, JSON.stringify(abi), signer)
     }
 
+    /**
+     * Once the general ledgers are set up, accounts need to be created for each party that the issuer or investor transacts with. 
+     * For example, a issuer can be a counter party for an investor. An investor who buys a bond from another investor will also 
+     * become its counter party. Accounts need to be created using the following solidity function using the party/counterparty’s address
+     * @param {address _account, bytes32 _currency} 
+     * @returns {address}
+     */
     public createAccount(params: CreateAccount): any {
         return this.callContract(FUNCTIONS.CREATEACCOUNT, params)
     }
