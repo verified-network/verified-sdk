843b228942abfc9697edd649b0947412969952a9
Achin <achin.k@rejolut.com>
2021-03-26 23:31:21 +0530

    updated VerifiedContract contract class

--- src/contract/index.ts
+++ src/contract/index.ts
@@ -1,6 +1,5 @@
 "use strict"
-
-import { ethers, utils } from "ethers";
+import { ethers, utils, ContractInterface, BigNumber } from "ethers";
 import { VerifiedWallet } from "../wallet";
 
 enum STATUS {
@@ -8,7 +7,7 @@ enum STATUS {
     ERROR
 }
 interface SCResponse {
-    data: object;
+    response: object;
     status: STATUS;
     message: string;
 }
@@ -17,34 +16,58 @@ export enum DATATYPES {
     STRING = 'string',
     ADDRESS = 'address',
     BOOLEAN = 'boolean',
-    BYTE32 = 'byte32'
+    BYTE32 = 'byte32',
+    BIGNUMBER = 'bignumber'
 }
+
+// const options = { gasLimit: 250000, gasPrice: 900000 }
 export class VerifiedContract {
 
     private signer: VerifiedWallet;
     private contract: ethers.Contract;
+    private abiInterface: ContractInterface;
+    private dataEncoder: any;
+    private dataDecoder: any;
+    private fragments: any
+    private functionFragments: any
+    protected abiCoder: any
 
     constructor(address: string, abi: string, signer: VerifiedWallet) {
         this.signer = signer;
-        this.contract = new ethers.Contract(address, abi, signer);
+        this.abiInterface = new utils.Interface(abi)
+        this.fragments = this.abiInterface.fragments
+        this.functionFragments = this.abiInterface.fragments
+        this.contract = new ethers.Contract(address, this.abiInterface, signer);
+        this.abiCoder = new utils.AbiCoder
+        this.dataEncoder = this.abiInterface.encodeFunctionData
+        this.dataDecoder = this.abiInterface.decodeFunctionData
+    }
+
+    protected createEncoder(functionName: string, ...args: any) {
+        return this.dataEncoder(...args)
+    }
+
+    protected createDecoder(functionName: string, ...args: any) {
+        console.log('createDecoder', functionName, ...args)
+        return this.dataDecoder(functionName, args)
     }
     /* ethers.utils.computeAddress( publicOrPrivateKey ) ⇒ string< Address >source
     Returns the address for publicOrPrivateKey. A public key may be compressed or uncompressed, and a private key will be converted automatically to a public key for the derivation. */
-    // public validateInputParams(functionName: string, _params: any) {
-    //     console.log(_params);
-    //     console.log("****************");
-    //     // let inputLength = 2;
-    //     // let params = _params.splice(0, inputLength);
-    //     let overrideOptions = _params.splice(- 1);
-    //     return { _params, overrideOptions };
-    // }
+    public validateInputParams(functionName: string, _params: any) {
+        console.log(_params);
+        console.log("****************");
+        // let inputLength = 2;
+        // let params = _params.splice(0, inputLength);
+        let overrideOptions = _params.splice(- 1);
+        return { _params, overrideOptions };
+    }
     protected async validateInput(type: DATATYPES, data: any) {
         // try {
         switch (type) {
             case DATATYPES.ADDRESS:
                 return utils.isAddress(data) ? true : new Error("Invalid Address is this");
             case DATATYPES.NUMBER:
-                return !Number.isNaN(parseInt(data))
+            // return !Number.isNaN(parseInt(data))
             case DATATYPES.BOOLEAN:
                 // const arr = [true, false, "true", "false"]
                 return typeof data === "boolean" ? true : new Error("Invalid Boolean value");
@@ -56,7 +79,7 @@ export class VerifiedContract {
         // }
     }
 
-    
+
 
     protected sanitiseInput(type: DATATYPES, data: any) {
         try {
@@ -108,10 +131,25 @@ export class VerifiedContract {
                  */
                 return utils.formatUnits(data)
 
+            case DATATYPES.BIGNUMBER:
+
+                return data.toString()
+
             default:
                 return data
 
         }
+    }
+
+    private tempOutput(data: any): object {
+        console.log('tempOutput', data);
+
+        const response: any = {}
+        if (data.hash) response['hash'] = data.hash
+        if (data._isBigNumber) response['result'] = [data.toString()]
+        if (typeof data === 'boolean') response['result'] = [data]
+
+        return response
     }
 
     async callContract(functionName: string, ...args: any) {
@@ -121,27 +159,36 @@ export class VerifiedContract {
             // let sanitisedParams = this.sanitiseInput(params);
             // console.log('*************',sanitisedParams)
 
-            const options = args.splice(-1)
+            let options = args.splice(-1)
+            console.log('options before', options);
+
+            if (options[0] === undefined) options = { options }
+            console.log('*********', ...args)
+            console.log('options after', options);
             // let params = [...args];
             // let options = params[params.length - 1];
             // console.log(typeof options);
             // options = typeof options === 'object' || typeof options === 'undefined' ? params.pop() : undefined;
-            // // let {params, overrideOptions} = this.validateInputParams(functionName, _params);
+            // let {params, overrideOptions} = this.validateInputParams(functionName, _params);
             // console.log("Params:: ", params, " Options:: ", options);
 
             /**
              * Actual Function call using Ethers.js
              */
+
             let fn = this.contract[functionName];
             // console.log(params)
-            let _res = await fn(...args, { ...options });
-            // console.log('_res', _res)
-            let _resp = _res.wait !== undefined ? await _res.wait(_res) : res;
+            let _res = await fn(...args, ...options);
+            // if (_res.transactionHash !== undefined) res['response']['transactionHash'] = _res.transactionHash
+
+            // console.log('_res', _res.toString())
+            // let _resp = this.createDecoder(functionName, _res)
+            // let _resp = _res.wait !== undefined ? await _res.wait(_res) : res;
             // let _resp = await _res.wait(_res);
             // console.log('_resp', _resp)
             // let { hash, nonce, to, chainId, result = [] } = await this.sanitiseOutput(_res);
 
-            res.data = _resp
+            res.response = this.tempOutput(utils.deepCopy(_res))
             res.status = STATUS.SUCCESS;
             res.message = '';
             return res;
@@ -149,8 +196,7 @@ export class VerifiedContract {
             console.error(error);
 
             res.status = STATUS.ERROR;
-            res.message = error.reason;
-            res.data = {};
+            res.message = error.reason !== undefined ? error.reason : error.code;
             return res;
         }
     }
