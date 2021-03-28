a515a30fa9ee6d5101d6b64544aa4f2ff28a5e5e
Kallol Borah <kallol.borah@gmail.com>
2021-03-28 17:48:57 +0530

    checking in account test cases

--- test/account.js
+++ test/account.js
@@ -3,27 +3,28 @@ var system = require('../wallet/register');
 
 //let client = '0x5469DD6891EE2aF053657e587A4Fd6A2E1a8608F'; // put public address of client 
 //let manager = '0x19836182a3786CD592523cAB7445325be26c3334'; //put public address of manager
-let client = '0xea9c03DA067A6e089A339438F64a3003AB613539'; // put account[1] of ganache
-let manager = '0xf5451893caC30E4B8774d39f42081A1269B88ef3'; // put account[0] of ganache
-let custodian = '0x0D6dA05B44De7694d2e8fAFF39634dCd8Dbe2112'; //put any address that is valid
+let client = '0x424185A8c72461Decf16B9C4bC870FDB3313CE58'; // put account[1] of ganache
+let manager = '0xC60869fDB1c02ea0a98E7dbBF3357418845B4972'; // put account[0] of ganache
+let custodian = '0x1718427c4A973CA99F49bd9310AA88aFEC1f5fbe'; //put any address that is valid
 
 console.log("User's wallet address is " + user.wallet.address);
 
-system.setManager(client, manager); //manager should not be client itself + manager should be deployer of contracts on the network
+//all test cases below need nonce to be incremented
 
-console.log("Set manager as " + system.getManager(client));
-/*
-system.setKycStatus(client, 'true'); //only manager can set KYC status
+system.setManager(client, "manager"); 
 
-console.log("Set status as "+system.getKycStatus(client));
+//console.log("Set manager as " + system.getManager(client));
 
-system.setAccess('true');
+//system.setKycStatus(client, 'true'); 
 
-console.log("Set access as "+system.getAccess(client));
+//console.log("Set status as "+system.getKycStatus(client));
 
-system.setCustodian(client, custodian);
+//system.setAccess('true');
 
-console.log("Set custodian as "+system.getCustodian);
+//console.log("Set access as "+system.getAccess(client));
 
-system.updateKycRecord(client, 'file', 'address', 'photo', 'video', 'fatca', 'crs');
-*/
+//system.setCustodian(client, custodian);
+
+//console.log("Set custodian as "+system.getCustodian(client));
+
+//system.updateKycRecord(client, 'file', 'address', 'photo', 'video', 'fatca', 'crs');
