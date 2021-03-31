c7fa262f71b77c0770a50c9014fa36003615db83
Achin <achin.k@rejolut.com>
2021-03-31 13:13:37 +0530

    updated ledger function

--- src/contract/ledger/index.ts
+++ src/contract/ledger/index.ts
@@ -5,6 +5,7 @@ import { VerifiedWallet } from "../../wallet";
 import { abi } from '../../abi/accounts/Ledger.json';
 import { contractAddress } from '../../contractAddress/index';
 import { CreateAccount } from '../../models/ledger';
+import { DATATYPES } from "../index";
 
 enum FUNCTIONS {
     CREATEACCOUNT = 'createAccount'
@@ -25,8 +26,8 @@ export default class LedgerContract extends VerifiedContract {
      * @param {address _account, bytes32 _currency} 
      * @returns {address}
      */
-    public createAccount(params: CreateAccount): any {
-        return this.callContract(FUNCTIONS.CREATEACCOUNT, params)
+    public createAccount(_accountName: string, _currency: string, options?: { gasPrice: number, gasLimit: number }): any {
+        return this.callContract(FUNCTIONS.CREATEACCOUNT, this.sanitiseInput(DATATYPES.BYTE32, _accountName), this.sanitiseInput(DATATYPES.BYTE32, _currency), options)
     }
 
 
