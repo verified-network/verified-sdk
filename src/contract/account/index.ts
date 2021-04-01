0577f5db375ccc727da8ddac70ece03a2cb3f4a9
Achin <achin.k@rejolut.com>
2021-04-01 22:04:58 +0530

    added validation in account contract functions

--- src/contract/account/index.ts
+++ src/contract/account/index.ts
@@ -4,6 +4,7 @@ import { VerifiedContract } from '../index';
 import { VerifiedWallet } from "../../wallet";
 import { abi } from '../../abi/accounts/System.json';
 import { contractAddress } from '../../contractAddress/index';
+import { DATATYPES } from "../index";
 import { PostEntry } from '../../models/account';
 
 enum FUNCTIONS {
@@ -21,10 +22,15 @@ export default class AccountContract extends VerifiedContract {
     /**
      * For each transaction, an account entry needs to be posted using the following solidity function.
      * Where, _vchType is voucher type and can be either ‘Journal’ , ‘Cash, ‘Bank, ‘Sale’ or ‘Purchase'.
-     * @param {address _account, bytes32 _accountNumber, int256 _txAmount, bytes32 _txType, bytes32 _txDate, bytes32 _txDescription, bytes32 _vchType} 
-     * @returns {address}
+     * @param (address _account, bytes32 _accountNumber, int256 _txAmount, bytes32 _txType, bytes32 _txDate, bytes32 _txDescription, bytes32 _vchType)
+     * 
      */
-    public postEntry(_accountAddress, _accountNumber, _txAmount, _txType, _txDate, _txDescription, _vchType, options?: { gasLimit, gasPrice }): any {
-        return this.callContract(FUNCTIONS.POSTENTRY, _accountAddress, _accountNumber, _txAmount, _txType, _txDate, _txDescription, _vchType, options)
+    public async postEntry(_accountAddress, _accountNumber, _txAmount, _txType, _txDate, _txDescription, _vchType, options?: { gasLimit, gasPrice }): any {
+
+        await this.validateInput(DATATYPES.ADDRESS, _accountAddress)
+        await this.validateInput(DATATYPES.STRING, _accountNumber)
+        await this.validateInput(DATATYPES.NUMBER, _txAmount)
+        
+        return this.callContract(FUNCTIONS.POSTENTRY, _accountAddress, _accountNumber, _txAmount, this.sanitiseInput(DATATYPES.BYTE32, _txType), this.sanitiseInput(DATATYPES.BYTE32, _txDate), this.sanitiseInput(DATATYPES.BYTE32, _txDescription), this.sanitiseInput(DATATYPES.BYTE32, _vchType), options)
     }
 }
