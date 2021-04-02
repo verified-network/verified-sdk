0577b74038a73bd2a866ca23ac4e0b733b760c2e
Achin <achin.k@rejolut.com>
2021-04-02 16:46:00 +0530

    added validation to cash functions

--- src/contract/cash/index.ts
+++ src/contract/cash/index.ts
@@ -23,10 +23,13 @@ export default class CashContract extends VerifiedContract {
      * For example, an investor can request a USD cash token by paying in a EUR cash token.
      * where, the sender is the address of cash token paid in, the receiver is the address of the cash token converted to
      * and the tokens parameter is a numeric specifying number of cash tokens paid in.
-     * @param {address sender, address receiver, uint256 tokens} 
-     * @returns {boolean}
+     * @param (address sender, address receiver, uint256 tokens)
+     * @returns boolean
      */
-    public transferFrom(params: TransferFrom): any {
-        return this.callContract(FUNCTIONS.TRANSFERFROM, params)
+     public transferFrom(_senderAddress: string, _recieverAddress: string, _tokens: number, options?: { gasPrice: number, gasLimit: number }): any {
+        await this.validateInput(DATATYPES.STRING, _senderAddress)
+        await this.validateInput(DATATYPES.STRING, _recieverAddress)
+        await this.validateInput(DATATYPES.NUMBER, _tokens)
+        return this.callContract(FUNCTIONS.TRANSFERFROM, _senderAddress, _recieverAddress, _tokens, options)
     }
 }
