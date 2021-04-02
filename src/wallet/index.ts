f13b9018b9b14c654420291ebe4b9598c91cce68
Achin <achin.k@rejolut.com>
2021-04-02 14:28:49 +0530

    added web3provider to connect from webbrowser

--- src/wallet/index.ts
+++ src/wallet/index.ts
@@ -1,7 +1,9 @@
+// @ts-nocheck
 "use strict"
 
 import { EtherWallet, ExternalAccount, VerifiedBytesLike } from './etherWallet';
 import { Provider } from "../utils/provider";
+import { ethers } from 'ethers';
 
 export class VerifiedWallet extends EtherWallet {
 
@@ -28,5 +30,18 @@ export class VerifiedWallet extends EtherWallet {
     setProvider(provider: Provider) {
         return new VerifiedWallet(this.privateKey, provider);
     }
-
+    /**
+     * The Metamask plugin also allows signing transactions to
+     * send ether and pay to change state within the blockchain.
+     * For this, we need the account signer...
+     * @param window.ethereum object 
+     * @returns signer object
+     */
+    static async web3Provider(object: any) {
+        if(!object.window.ethereum.isMetaMask) return new Error('Invalid object')
+        await window.ethereum.enable()
+        const provider = new ethers.providers.Web3Provider(object)
+        const signer = provider.getSigner()
+        return signer
+    }
 }
