bc3123b798384fafc9044b105fd87fa99423fb99
Achin <achin.k@rejolut.com>
2021-04-02 16:45:38 +0530

    added validation to bond functions

--- src/contract/bond/index.ts
+++ src/contract/bond/index.ts
@@ -4,10 +4,10 @@ import { VerifiedContract } from '../index';
 import { VerifiedWallet } from "../../wallet";
 import { abi } from '../../abi/accounts/System.json';
 import { contractAddress } from '../../contractAddress/index';
-import {  RequestIssue } from '../../models/bond';
+import { DATATYPES } from "../index";
 
 enum FUNCTIONS {
-        REQUESTISSUE = 'requestIssue'
+    REQUESTISSUE = 'requestIssue'
 }
 
 export default class BondContract extends VerifiedContract {
@@ -22,10 +22,27 @@ export default class BondContract extends VerifiedContract {
      * Also, just as in the case of cash tokens, an investor can pay in a cash token and purchase bond tokens. 
      * For this, the solidity function to be called where, payer is the investor’s address, currency is cash token paid in, 
      * cashContract is the address of the cash token paid in, and amount is the numeric of amount in currency paid in.
-     * @param {bytes16 amount, address payer, bytes32 currency, address cashContract} 
-     * @returns {boolean}
+     * @param (bytes16 amount, address payer, bytes32 currency, address cashContract)
+     * @returns boolean
      */
-    public requestIssue(params: RequestIssue): any {
-        return this.callContract(FUNCTIONS.REQUESTISSUE, params)
+    public requestIssue(_amount: number, _payerAddress: string, _currency: string, _cashContractAddress: string, options?: { gasPrice: number, gasLimit: number }): any {
+        await this.validateInput(DATATYPES.NUMBER, _amount)
+        await this.validateInput(DATATYPES.STRING, _payerAddress)
+        await this.validateInput(DATATYPES.STRING, _currency)
+        await this.validateInput(DATATYPES.STRING, _cashContractAddress)
+        return this.callContract(FUNCTIONS.REQUESTISSUE, params, options)
+    }
+
+    /**
+     * For bond redemption, the following solidity function needs to be called. where, sender is the investor’s address, receiver is the issuing investor’s address, and tokens are numeric amount of bond tokens to redeem
+     * @param params 
+     * @param options 
+     * @returns 
+     */
+    public transferFrom(_senderAddress: string, _recieverAddress: string, _tokens: number, options?: { gasPrice: number, gasLimit: number }): any {
+        await this.validateInput(DATATYPES.STRING, _senderAddress)
+        await this.validateInput(DATATYPES.STRING, _recieverAddress)
+        await this.validateInput(DATATYPES.NUMBER, _tokens)
+        return this.callContract(FUNCTIONS.TRANSFERFROM, _senderAddress, _recieverAddress, _tokens, options)
     }
 }
