d439425b92561ff09a168afae02c24108fa195b9
Kallol Borah <kallol.borah@gmail.com>
2022-01-27 23:07:39 +0530

    updated sdk v0.5.4 with changes in creating, editing orders and getting bonds issued, purchased

--- src/contract/orderpool/index.ts
+++ src/contract/orderpool/index.ts
@@ -30,15 +30,14 @@ export default class OrderPoolContract extends VerifiedContract {
    * @param (address _security, address _cash, uint256 _price, uint256 _trigger, uint256 _qty, bytes32 _orderType, bytes32 _order)
    * @returns (bytes32)
    */
-  public async newOrder(_security: string, _cash: string, _price: string, _trigger: string, _qty: string, _orderType: string, _order: string, options?: { gasPrice: number, gasLimit: number }): any {
+  public async newOrder(_security: string, _cash: string, _price: string, _qty: string, _orderType: string, _order: string, options?: { gasPrice: number, gasLimit: number }): any {
     await this.validateInput(DATATYPES.ADDRESS, _security)
     await this.validateInput(DATATYPES.ADDRESS, _cash)
     await this.validateInput(DATATYPES.STRING, _price)
-    await this.validateInput(DATATYPES.STRING, _trigger)
     await this.validateInput(DATATYPES.STRING, _qty)
     await this.validateInput(DATATYPES.STRING, _orderType)
     await this.validateInput(DATATYPES.STRING, _order)
-    return this.callContract(FUNCTIONS.NEWORDER, _security, _cash, _price, _trigger, _qty, this.sanitiseInput(DATATYPES.BYTE32, _orderType), this.sanitiseInput(DATATYPES.BYTE32, _order), options)
+    return this.callContract(FUNCTIONS.NEWORDER, _security, _cash, _price, _qty, this.sanitiseInput(DATATYPES.BYTE32, _orderType), this.sanitiseInput(DATATYPES.BYTE32, _order), options)
   }
 
 
@@ -47,14 +46,11 @@ export default class OrderPoolContract extends VerifiedContract {
    * @param ( bytes32 _ref, uint256 _price, uint256 _trigger, uint256 _qty, bytes32 _orderType, bytes32 _order)
    * @returns (bool)
    */
-  public async editOrder(_ref: string, _price: string, _trigger: string, _qty: string, _orderType: string, _order: string, options?: { gasPrice: number, gasLimit: number }): any {
+  public async editOrder(_ref: string, _price: string, _qty: string, options?: { gasPrice: number, gasLimit: number }): any {
     await this.validateInput(DATATYPES.STRING, _ref)
     await this.validateInput(DATATYPES.STRING, _price)
-    await this.validateInput(DATATYPES.STRING, _trigger)
     await this.validateInput(DATATYPES.STRING, _qty)
-    await this.validateInput(DATATYPES.STRING, _orderType)
-    await this.validateInput(DATATYPES.STRING, _order)
-    return this.callContract(FUNCTIONS.EDITORDER, _ref, _price, _trigger, _qty, this.sanitiseInput(DATATYPES.BYTE32, _orderType), this.sanitiseInput(DATATYPES.BYTE32, _order), options)
+    return this.callContract(FUNCTIONS.EDITORDER, _ref, _price, _qty, options)
   }
 
 
