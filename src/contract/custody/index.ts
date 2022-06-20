f5abe6a95a023dc60d489549c242dc3506f1a5a6
Kallol Borah <kallol.borah@gmail.com>
2022-06-20 12:58:04 +0530

    updated custody ABIs and function signatures in SDK 0.8.6

--- src/contract/custody/index.ts
+++ src/contract/custody/index.ts
@@ -34,7 +34,7 @@ export default class CustodyContract extends VerifiedContract {
     public async createVault(_creator: string, _id: string, options?: { gasPrice: number, gasLimit: number }): any {
         await this.validateInput(DATATYPES.STRING, _creator)
         await this.validateInput(DATATYPES.STRING, _id)
-        return this.callContract(FUNCTIONS.CREATEVAULT, this.sanitiseInput(DATATYPES.BYTE32, _creator), this.sanitiseInput(DATATYPES.BYTE32, _id), options)
+        return this.callContract(FUNCTIONS.CREATEVAULT, this.sanitiseInput(DATATYPES.BYTE32, _creator), _id, options)
     }
 
     public async getCreator(_creator: string, options?: { gasPrice: number, gasLimit: number }): any {
@@ -42,60 +42,53 @@ export default class CustodyContract extends VerifiedContract {
         return this.callContract(FUNCTIONS.GETCREATOR, this.sanitiseInput(DATATYPES.BYTE32, _creator), options)
     }
 
-    public async addParticipant(_creator: string, _id: string, _participant: string, options?: { gasPrice: number, gasLimit: number }): any {
-        await this.validateInput(DATATYPES.STRING, _creator)
-        await this.validateInput(DATATYPES.STRING, _id)
-        await this.validateInput(DATATYPES.STRING, _participant)
-        return this.callContract(FUNCTIONS.ADDPARTICIPANT, this.sanitiseInput(DATATYPES.BYTE32, _creator), this.sanitiseInput(DATATYPES.BYTE32, _id), this.sanitiseInput(DATATYPES.BYTE32, _participant), options)
-    }
-
-    public async removeParticipant(_creator: string, _id: string, _participant: string, options?: { gasPrice: number, gasLimit: number }): any {
-        await this.validateInput(DATATYPES.STRING, _creator)
-        await this.validateInput(DATATYPES.STRING, _id)
-        await this.validateInput(DATATYPES.STRING, _participant)
-        return this.callContract(FUNCTIONS.REMOVEPARTICIPANT, this.sanitiseInput(DATATYPES.BYTE32, _creator), this.sanitiseInput(DATATYPES.BYTE32, _id), this.sanitiseInput(DATATYPES.BYTE32, _participant), options)
-    }
-
-    public async confirmParticipant(_creator: string, _participant: string, _id: string, _shard: string, _pin:string, options?: { gasPrice: number, gasLimit: number }): any {
-        await this.validateInput(DATATYPES.STRING, _creator)
-        await this.validateInput(DATATYPES.STRING, _id)
+    public async addParticipant(_creator: string, _participant: string, _shard: string, options?: { gasPrice: number, gasLimit: number }): any {
+        await this.validateInput(DATATYPES.STRING, _creator)        
         await this.validateInput(DATATYPES.STRING, _participant)
         await this.validateInput(DATATYPES.STRING, _shard)
-        return this.callContract(FUNCTIONS.CONFIRMPARTICIPANT, this.sanitiseInput(DATATYPES.BYTE32, _creator), this.sanitiseInput(DATATYPES.BYTE32, _participant), this.sanitiseInput(DATATYPES.BYTE32, _id),  _shard, _pin, options)
+        return this.callContract(FUNCTIONS.ADDPARTICIPANT, this.sanitiseInput(DATATYPES.BYTE32, _creator), this.sanitiseInput(DATATYPES.BYTE32, _participant), _shard, options)
     }
 
-    public async defineQuorum(_creator: string, _id: string, _minParticipants:string, options?: { gasPrice: number, gasLimit: number }): any {
+    public async removeParticipant(_creator: string, _participant: string, options?: { gasPrice: number, gasLimit: number }): any {
         await this.validateInput(DATATYPES.STRING, _creator)
-        await this.validateInput(DATATYPES.STRING, _id)
-        return this.callContract(FUNCTIONS.DEFINEQUORUM, this.sanitiseInput(DATATYPES.BYTE32, _creator), this.sanitiseInput(DATATYPES.BYTE32, _id), _minParticipants, options)
-    }
-
-    public async promptSignatures(_creator: string, _id: string, options?: { gasPrice: number, gasLimit: number }): any {
-        await this.validateInput(DATATYPES.STRING, _creator)
-        await this.validateInput(DATATYPES.STRING, _id)
-        return this.callContract(FUNCTIONS.PROMPTSIGNATURES, this.sanitiseInput(DATATYPES.BYTE32, _creator), this.sanitiseInput(DATATYPES.BYTE32, _id), options)
-    }
-
-    public async signTransaction(_creator: string, _participant: string, _id: string, _tx: string, _pin:string, options?: { gasPrice: number, gasLimit: number }): any {
-        await this.validateInput(DATATYPES.STRING, _creator)
-        await this.validateInput(DATATYPES.STRING, _id)
         await this.validateInput(DATATYPES.STRING, _participant)
-        return this.callContract(FUNCTIONS.SIGNTRANSACTION, this.sanitiseInput(DATATYPES.BYTE32, _creator), this.sanitiseInput(DATATYPES.BYTE32, _participant), this.sanitiseInput(DATATYPES.BYTE32, _id),  _tx, _pin, options)
+        return this.callContract(FUNCTIONS.REMOVEPARTICIPANT, this.sanitiseInput(DATATYPES.BYTE32, _creator), this.sanitiseInput(DATATYPES.BYTE32, _participant), options)
     }
 
-    public async checkQuorum(_creator: string, _id: string, _participant: string, _txid: string, options?: { gasPrice: number, gasLimit: number }): any {
+    public async confirmParticipant(_creator: string, _participant: string, _pin:string, options?: { gasPrice: number, gasLimit: number }): any {
         await this.validateInput(DATATYPES.STRING, _creator)
-        await this.validateInput(DATATYPES.STRING, _id)
+        await this.validateInput(DATATYPES.STRING, _participant)
+        return this.callContract(FUNCTIONS.CONFIRMPARTICIPANT, this.sanitiseInput(DATATYPES.BYTE32, _creator), this.sanitiseInput(DATATYPES.BYTE32, _participant), _pin, options)
+    }
+
+    public async defineQuorum(_creator: string, _minParticipants:string, options?: { gasPrice: number, gasLimit: number }): any {
+        await this.validateInput(DATATYPES.STRING, _creator)
+        await this.validateInput(DATATYPES.NUMBER, _minParticipants)
+        return this.callContract(FUNCTIONS.DEFINEQUORUM, this.sanitiseInput(DATATYPES.BYTE32, _creator), _minParticipants, options)
+    }
+
+    public async promptSignatures(_creator: string, options?: { gasPrice: number, gasLimit: number }): any {
+        await this.validateInput(DATATYPES.STRING, _creator)
+        return this.callContract(FUNCTIONS.PROMPTSIGNATURES, this.sanitiseInput(DATATYPES.BYTE32, _creator), options)
+    }
+
+    public async signTransaction(_creator: string, _participant: string, _tx: string, _pin:string, options?: { gasPrice: number, gasLimit: number }): any {
+        await this.validateInput(DATATYPES.STRING, _creator)
+        await this.validateInput(DATATYPES.STRING, _participant)
+        return this.callContract(FUNCTIONS.SIGNTRANSACTION, this.sanitiseInput(DATATYPES.BYTE32, _creator), this.sanitiseInput(DATATYPES.BYTE32, _participant), _tx, _pin, options)
+    }
+
+    public async checkQuorum(_creator: string, _participant: string, _txid: string, options?: { gasPrice: number, gasLimit: number }): any {
+        await this.validateInput(DATATYPES.STRING, _creator)
         await this.validateInput(DATATYPES.STRING, _participant)
         await this.validateInput(DATATYPES.STRING, _txid)
-        return this.callContract(FUNCTIONS.CHECKQUORUM, this.sanitiseInput(DATATYPES.BYTE32, _creator), this.sanitiseInput(DATATYPES.BYTE32, _id), this.sanitiseInput(DATATYPES.BYTE32, _participant), this.sanitiseInput(DATATYPES.BYTE32, _txid), options)
+        return this.callContract(FUNCTIONS.CHECKQUORUM, this.sanitiseInput(DATATYPES.BYTE32, _creator), this.sanitiseInput(DATATYPES.BYTE32, _participant), this.sanitiseInput(DATATYPES.BYTE32, _txid), options)
     }
 
-    public async getShards(_creator: string, _id: string, _txid: string, options?: { gasPrice: number, gasLimit: number }): any {
+    public async getShards(_creator: string, _txid: string, options?: { gasPrice: number, gasLimit: number }): any {
         await this.validateInput(DATATYPES.STRING, _creator)
-        await this.validateInput(DATATYPES.STRING, _id)
         await this.validateInput(DATATYPES.STRING, _txid)
-        return this.callContract(FUNCTIONS.GETSHARDS, this.sanitiseInput(DATATYPES.BYTE32, _creator), this.sanitiseInput(DATATYPES.BYTE32, _id), this.sanitiseInput(DATATYPES.BYTE32, _txid), options)
+        return this.callContract(FUNCTIONS.GETSHARDS, this.sanitiseInput(DATATYPES.BYTE32, _creator), this.sanitiseInput(DATATYPES.BYTE32, _txid), options)
     }
 
 }
