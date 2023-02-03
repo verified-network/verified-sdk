4ef7f97f2769d21e44bbd639b162c5f6c7146fca
Kallol Borah <kallol.borah@gmail.com>
2023-02-03 08:11:26 +0530

    updated with secondary issue pool abi for separate limit order function

--- dist/contractAddress/index.js
+++ dist/contractAddress/index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const contractAddress = {
     'L1-goerli': {
-        'Client': '0xa9C748654A33BB58F4950FC7e5C79475aee2cbCa',
+        'Client': '0x0181D33D6204c940f0D58670EFc88D9a829D6B55',
         'KYC': '',
         'System': '',
         'Holder': '',
@@ -26,8 +26,8 @@ const contractAddress = {
         'Liquidity': '0x9Ab60Ce0ae460Cfa7961858c0065034e35e009FD',
         'Vitta': '0xB40716004C92C8B6C65DA47b8073089027C189D1',
         'Distribution': '0x721965FAf93C94EeB1479F649f3c6ce67b0108D2',
-        'BalancerPrimaryIssueManager': '0x98080B625fa0b4F00FbC9F7459d295b527aB2A00',
-        'BalancerSecondaryIssueManager': '0x6460FCD6C29804557FA88a441108382005cFE29b',
+        'BalancerPrimaryIssueManager': '0xcf026775ac0a80f7AeedC79Dd3E5Ae5b8e895350',
+        'BalancerSecondaryIssueManager': '0xc2945D10408f6Ba929D6b0dE4Bc5d20439112ea3',
         'Custody': '0xF6ebB33E758eC36A2D3065F4633131DFF8307A07',
         'CASH': {
             'VCUSD': '0x441059c70c8294BecC1796275507C93Ea61db12B',
