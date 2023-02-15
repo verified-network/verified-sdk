ba3f4bfe95a2ff98e9c2f92fa1a2d84fe4babebc
Kallol Borah <kallol.borah@gmail.com>
2023-02-15 08:37:53 +0530

    fixing scaling issues with fees for primary and secondary issues

--- dist/contractAddress/index.js
+++ dist/contractAddress/index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const contractAddress = {
     'L1-goerli': {
-        'Client': '0x87a31cC80f3e2dA1C99e1B547Df7Df0270E4d34b',
+        'Client': '0x98aB6edd7909005988537408881580257d551368',
         'KYC': '',
         'System': '',
         'Holder': '',
@@ -26,8 +26,8 @@ const contractAddress = {
         'Liquidity': '0x9Ab60Ce0ae460Cfa7961858c0065034e35e009FD',
         'Vitta': '0xB40716004C92C8B6C65DA47b8073089027C189D1',
         'Distribution': '0x721965FAf93C94EeB1479F649f3c6ce67b0108D2',
-        'BalancerPrimaryIssueManager': '0xA293FB03a8F548876F0a3Da40f2f56773351D9D9',
-        'BalancerSecondaryIssueManager': '0x56fe4b89Df69217aab0272f64E4fB2A6C010a048',
+        'BalancerPrimaryIssueManager': '0xbD23B3B9AA4A0b951511598EBD5395a6FD11A416',
+        'BalancerSecondaryIssueManager': '0xdDa57Cec2FfDd80C32C7Bb223cdE2EB9E8599066',
         'Custody': '0xF6ebB33E758eC36A2D3065F4633131DFF8307A07',
         'CASH': {
             'VCUSD': '0x441059c70c8294BecC1796275507C93Ea61db12B',
