d3ec16dc5f5729bccc4f5a1445a47137b555dfe2
Kallol Borah <kallol.borah@gmail.com>
2023-11-04 17:51:10 +0530

    updated sdk 1.3.5 with fixes to margin pool

--- dist/contractAddress/index.js
+++ dist/contractAddress/index.js
@@ -3,33 +3,33 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const contractAddress = {
     5: {
         'Client': '0x3e132ee9fc32428655b2151A4E6C3f2B1FD49923',
-        'Factory': '0xFA2ADf68Eb4fc9d4331Df124B5902021A78D2842',
-        'Cash': '0x9F6807CeeE8951d46ce33D57eEe3256B523bE56A',
-        'Bond': '0xeCd032f9Da2aA83d90D14D4648424422c4515216',
-        'Token': '0x8F82bC814C1b205e610d379e983aaDe72Ef6F9E2',
-        'Oracle': '0x5b42b50012607Ff072A625835eBB5Fd7D4Af90f9',
-        'Rates': '0x165Ca3C823FEf1EC9360b323FE7b0f2C7041cf51',
+        'Factory': '0xC4039Ad7465EE7c80B00658ee510e5000aB7baD3',
+        'Cash': '0xb879A9C3971B69223c5c87C18fd42F622CcBB16c',
+        'Bond': '0x148CDa5F8D299D719b5FAc7826E11A00785AAa3a',
+        'Token': '0xD84bD15B0F2c838d4A7F598553B189f0EcD39d18',
+        'Oracle': '0x01acc738C0371b51caF7e67A42cf6E423E04885c',
+        'Rates': '0x164E22A9Fea6A26FFE827F733d7c81f397241633',
         'Security': '0x93527FaeCD88c108C3Fb78a1Ca72536eeb1Adbf7',
         'SecuritiesFactory': '0x647a7EA364a31fa18c65f4043CD55718d245D774',
         'Vitta': '0x8aa063d709A7Ec84086A0b5330c75D73af528cD3',
         'Liquidity': '0xce6FD14dD748F5f371BB226a3FdF6fEA3a4C423B',
         'Distribution': '0x22Af8F976f34a5f91A333bcc2bBe82826e517B12',
-        'BalancerPrimaryIssueManager': '0x63D95635938857Ad202f0684dfd91dc71C9d111e',
-        'BalancerSecondaryIssueManager': '0xe1f45e7104069F998b919C26435c4aaBAeD68fEd',
-        'BalancerMarginIssueManager': '0x6Aaee8a95a364e208E6915805537437463EF2Cdd',
+        'BalancerPrimaryIssueManager': '0xa1582D9848cd72cfd001b32Ae7872A9de4E8f695',
+        'BalancerSecondaryIssueManager': '0xd99eaA9786050dab5c3332E1E6eD9d6C4fB6cb6e',
+        'BalancerMarginIssueManager': '0xb280001FEb2dd788Cc1D8F04677Dd94884512538',
         'Custody': '0x60aEDad13a34b1DC45bDeA9eD6a66d639563e917',
         'Compound': '0xf8287e8D37863Db0Cabd25B55f77652d5d6aEb83',
         'CASH': {
-            'VCUSD': '0xa6aa25115f23F3ADc4471133bbDC401b613DbF65',
-            'VCEUR': '0xEeD0099ad11960f963Bfdf359c79Acb75E27C5d3',
-            'VCCHF': '0xdB551DA261786abcF637Cff6c4E64d2CBe5BB3CA',
-            'VCINR': '0x255744717B3b01BBF9388FEb7fa829D1A5b44ba8'
+            'VCUSD': '0xed40C75d273a8f9c746536b86d7B849871970E9D',
+            'VCEUR': '0xeFbDD964114aECf26fBAc495551697C1B20016b5',
+            'VCCHF': '0x2C0Aa071b73E6b76026Cf6a9D2D2863A0eCA1080',
+            'VCINR': '0x64B859Fc4c082b07d77A493dcB024926Bc47306a'
         },
         'BOND': {
-            'VBUSD': '0x0AD3B8eA3F464d0C6DaafC630f50Fffa0b860E73',
-            'VBEUR': '0x388212CE38453dcE7C63f80E27fe2C74F96aaa3a',
-            'VCCHF': '0x5050553d73526D252c3465D36768E504BC9276d4',
-            'VBINR': '0xC9DC7B9A25e60627881912b19E25856452130BE4'
+            'VBUSD': '0xc0CBc97E0D940Df8d32E5F5c12eEaddC7Bfc1377',
+            'VBEUR': '0x86FFFDA97C7015870f59886536f8BA083011460e',
+            'VCCHF': '0x079c51F7BeA0326B44EB95C7a6F45aC067b2d52b',
+            'VBINR': '0x699Da71a435336238F1081aD46448CfbCD18c380'
         },
     },
     'verified': {
