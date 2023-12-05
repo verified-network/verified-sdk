7fde68a5e4aad0dafcca580c7cc71b9468622c80
Mohzcrea8me <mohzcrea8me@gmail.com>
2023-12-05 14:20:44 +0800

    feat: test wait time reduction

--- src/test/securityFactoryGasless.spec.ts
+++ src/test/securityFactoryGasless.spec.ts
@@ -1,7 +1,7 @@
 import assert from 'assert';
 import { Provider } from "../utils/index";
 import { VerifiedWallet } from "../wallet/index";
-import { SecuritiesFactory, Security, contractAddress } from "../index";
+import { SecuritiesFactory, Security } from "../index";
 import { ethers } from "ethers";
 
 describe("Polygon Mumbai Testnet Tests", () => {
@@ -10,14 +10,15 @@ describe("Polygon Mumbai Testnet Tests", () => {
     "correct galaxy various swap chair assault blue improve ivory pear infant oak";
   const sender = VerifiedWallet.importWallet(SECURITY_HOLDER_MNEMONICS);
   const signer = sender.setProvider(Provider.infuraProvider(80001, INFURA_API_KEY));
-  describe("SecurityFactories Test", () => {
+  describe("SecurityFactories Gasless Test", () => {
     const securityFactoryAddress = "0xf1f349C2CBDA5BCAfD7F95b20C812b4A17c9333D";
     const securityFactoryContract = new SecuritiesFactory(
       signer,
       securityFactoryAddress
     );
     const zeroAddress = "0x0000000000000000000000000000000000000000";
-    it("it should Issue Security, add balance(mint 100) security token to user's account and transfer 1 security token from user account to another account", async () => {
+
+    it("it should Issue Security", async () => {
       const BTCShortBytes = "0x425443";
       const currency = "0x07865c6E87B9F70255377e024ace6630C1Eaa37F"; //usdc
       const defaultBytes =
@@ -45,57 +46,50 @@ describe("Polygon Mumbai Testnet Tests", () => {
             "Issued Security successfully with transaction hash: ",
             hash
           );
-          assert.notEqual(hash.length, 0);
-          if(hash.length > 0) {
-            securityFactoryContract.notifySecuritiesAdded(async (evnt: any) => {
-              const security = evnt.response.result[0] || "";
-              console.log("added security : ", security, "succesfully");
-              assert.notEqual(security.length, 0);
-              if (security !== "") {
-                it(`should test add Balance(mint 100) of ${security} security token `, async() => {
-                  await securityFactoryContract
-                  .addBalance(
-                    security,
-                    zeroAddress,
-                    signer.address,
-                    "1000000000000000000000"
-                  )
-                  .then(async (res: any) => {
-                    let addBalanceHash = res.response.hash || "";
-                    console.log(
-                      "Security Minted succesfully with hash: ",
-                      addBalanceHash
-                    );
-                    assert.notEqual(addBalanceHash.length, 0);
-                    if(addBalanceHash.length > 0) {
-                      it(`should test sending 1 ${security} token`, async () => {
-                        const securityContract = new Security(signer, security);
-                      const receiverAddress = "0x286a759DACfd0C533B88E42b9e7571040008D778";
-                      const amount = "1000000000000000000";
-                      await securityContract
-                        .transfer(receiverAddress, amount)
-                        .then((res: any) => {
-                          let transferHash = res.response.hash || "";
-                          console.log("Transfer succesful with hash: ", transferHash);
-                          assert.notEqual(transferHash, 0);
-                        })
-                        .catch((err: any) => {
-                          console.error("Transfer failed with error: ", err);
-                        });
-                      })
-                    }
-                  })
-                  .catch((err: any) => {
-                    console.error("Mint security failed with error: ", err);
-                  });
-                })
-              }
-            });
-          }
+          assert.notEqual(hash.length, 0)
         })
         .catch((err: any) => {
           console.error("IssueSecurity failed with error: ", err);
         });
     });
+
+    it(`should add Balance(mint 100) security tokens to user's wallet `, async() => {
+      const securityIssued = "0x46247bd421989de3beec138f37db892c8adfdf9c"
+      await securityFactoryContract
+      .addBalance(
+        securityIssued,
+        zeroAddress,
+        signer.address,
+        "1000000000000000000000"
+      )
+      .then(async (res: any) => {
+        let addBalanceHash = res.response.hash || "";
+        console.log(
+          "Security Minted succesfully with hash: ",
+          addBalanceHash
+        );
+        assert.notEqual(addBalanceHash.length, 0)
+      })
+      .catch((err: any) => {
+        console.error("Mint security failed with error: ", err);
+      });
+    })
+
+    it(`should test sending 1 security token from callers's wallet`, async () => {
+      const securityIssued = "0x46247bd421989de3beec138f37db892c8adfdf9c"
+          const securityContract = new Security(signer, securityIssued);
+          const receiverAddress = "0x286a759DACfd0C533B88E42b9e7571040008D778";
+          const amount = "1000000000000000000";
+            await securityContract
+            .transfer(receiverAddress, amount)
+            .then((res: any) => {
+              let transferHash = res.response.hash || "";
+              console.log("Transfer succesful with hash: ", transferHash);
+              assert.notEqual(transferHash, 0);
+            })
+            .catch((err: any) => {
+              console.error("Transfer failed with error: ", err);
+            });
+    })
   });
 });
