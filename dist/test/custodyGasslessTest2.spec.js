c75f46c4516c9ede76ad9d1cdfa80a3d91212a19
Kallol Borah <kallol.borah@gmail.com>
2024-05-14 19:39:04 +0200

    updated sdk v1.6.0 for base sepolia

--- dist/test/custodyGasslessTest2.spec.js
+++ dist/test/custodyGasslessTest2.spec.js
@@ -1,25 +1,23 @@
 "use strict";
-// import assert from 'assert';
-// import { Provider } from "../utils/index";
-// import { VerifiedWallet } from "../wallet/index";
-// import {Custody} from "../index";
-// describe("Sepolia(11155111) Custody Gassless Tests", () => {      
-//   const CREATOR_MNEMMONICS =
-//     "omit floor sport lend knock alpha dumb three gentle hungry screen early";
-//   const INFURA_API_KEY =   "e0951a7b82c84a6880f3c6f7bb182e21"
-//   const sender = VerifiedWallet.importWallet(CREATOR_MNEMMONICS);
-//   const provider = Provider.infuraProvider(11155111, INFURA_API_KEY)
-//   const signer = sender.setProvider(provider);
-//   describe("Tests some functions on custody contract", () => {
-//     const custodyAddress = "0x7aE9f79067AB4FDc8d41B18f1e6491590ac76f9d";
-//     const custodyContract = new Custody(
-//       signer,
-//       custodyAddress
-//     );
-//     it("it should create vault", async () => {
-//       const createVaultRes = await custodyContract.createVault("0x7665726966696564", "vltt");
-//       console.log("result: ", createVaultRes)
-//       assert(createVaultRes.response.status === 0);
-//     })
-//   });
-// });
+Object.defineProperty(exports, "__esModule", { value: true });
+const tslib_1 = require("tslib");
+const assert_1 = tslib_1.__importDefault(require("assert"));
+const index_1 = require("../utils/index");
+const index_2 = require("../wallet/index");
+const index_3 = require("../index");
+describe("Sepolia(11155111) Custody Gassless Tests", () => {
+    const CREATOR_MNEMMONICS = "omit floor sport lend knock alpha dumb three gentle hungry screen early";
+    const INFURA_API_KEY = "e0951a7b82c84a6880f3c6f7bb182e21";
+    const sender = index_2.VerifiedWallet.importWallet(CREATOR_MNEMMONICS);
+    const provider = index_1.Provider.infuraProvider(11155111, INFURA_API_KEY);
+    const signer = sender.setProvider(provider);
+    describe("Tests some functions on custody contract", () => {
+        const custodyAddress = "0x7aE9f79067AB4FDc8d41B18f1e6491590ac76f9d";
+        const custodyContract = new index_3.Custody(signer, custodyAddress);
+        it("it should create vault", async () => {
+            const createVaultRes = await custodyContract.createVault("0x7665726966696564", "vltt");
+            console.log("result: ", createVaultRes);
+            (0, assert_1.default)(createVaultRes.response.status === 0);
+        });
+    });
+});
--- dist/test/readFunctions.spec.js
+++ dist/test/readFunctions.spec.js
@@ -1,35 +1,33 @@
 "use strict";
-// import assert from 'assert';
-// import { Provider } from "../utils/index";
-// import { VerifiedWallet } from "../wallet/index";
-// import { Liquidity, PrimaryIssueManager } from "../index";
-// describe("Test to check new tempOutout fix on sepolia chain(11155111)", () => {
-//   const INFURA_API_KEY = "e0951a7b82c84a6880f3c6f7bb182e21";        
-//   const SIGNER_MNEMONICS =
-//     "correct galaxy various swap chair assault blue improve ivory pear infant oak";
-//   const sender = VerifiedWallet.importWallet(SIGNER_MNEMONICS);
-//   const signer = sender.setProvider(Provider.infuraProvider(11155111, INFURA_API_KEY));
-//   const liquidityContractAddress = "0x27006b68b3594EF5Ae04C5457c24F0c7CF1E5553";
-//   const primaryManagerAddress = "0x41bB86106CC5156d915052c3a3EFb4be70Ec544E";
-//   const currencyOffered = "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238" //sepolia(11155111) USDC
-//   //test using getSupportedTokens on liquidity contract and getOffered on primaryIssueManager contract due to dapp error
-//   describe("getSupportedTokens Test", () => {
-//     const liquidityContract = new Liquidity(
-//         signer,
-//         liquidityContractAddress
-//       );
-//     it("it call getSupportedTokens on liquidity contract and return response without error(s)", async () => {
-//       const getSupportedTokenRes = await liquidityContract.getSupportedTokens();
-//       console.log("get supported tokens response: ", getSupportedTokenRes.response) //log response to better explain
-//       assert(getSupportedTokenRes.status === 0) //check no error(s)
-//     });
-//   });
-//   describe("getOffered Test", () => {
-//     const primarymanagerContract = new PrimaryIssueManager(signer, primaryManagerAddress, "balancer");
-//     it("it call getOffered on balancer primaryissue manager contract and return response without error(s)", async () => {
-//         const getOfferedRes = await primarymanagerContract.getOffered(currencyOffered);
-//         console.log("get offered response: ", getOfferedRes.response.result) //log response
-//         assert(getOfferedRes.status === 0) //check no error(s)
-//     });
-//   });
-// });
+Object.defineProperty(exports, "__esModule", { value: true });
+const tslib_1 = require("tslib");
+const assert_1 = tslib_1.__importDefault(require("assert"));
+const index_1 = require("../utils/index");
+const index_2 = require("../wallet/index");
+const index_3 = require("../index");
+describe("Test to check new tempOutout fix on sepolia chain(11155111)", () => {
+    const INFURA_API_KEY = "e0951a7b82c84a6880f3c6f7bb182e21";
+    const SIGNER_MNEMONICS = "correct galaxy various swap chair assault blue improve ivory pear infant oak";
+    const sender = index_2.VerifiedWallet.importWallet(SIGNER_MNEMONICS);
+    const signer = sender.setProvider(index_1.Provider.infuraProvider(11155111, INFURA_API_KEY));
+    const liquidityContractAddress = "0x27006b68b3594EF5Ae04C5457c24F0c7CF1E5553";
+    const primaryManagerAddress = "0x41bB86106CC5156d915052c3a3EFb4be70Ec544E";
+    const currencyOffered = "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238"; //sepolia(11155111) USDC
+    //test using getSupportedTokens on liquidity contract and getOffered on primaryIssueManager contract due to dapp error
+    describe("getSupportedTokens Test", () => {
+        const liquidityContract = new index_3.Liquidity(signer, liquidityContractAddress);
+        it("it call getSupportedTokens on liquidity contract and return response without error(s)", async () => {
+            const getSupportedTokenRes = await liquidityContract.getSupportedTokens();
+            console.log("get supported tokens response: ", getSupportedTokenRes.response); //log response to better explain
+            (0, assert_1.default)(getSupportedTokenRes.status === 0); //check no error(s)
+        });
+    });
+    describe("getOffered Test", () => {
+        const primarymanagerContract = new index_3.PrimaryIssueManager(signer, primaryManagerAddress, "balancer");
+        it("it call getOffered on balancer primaryissue manager contract and return response without error(s)", async () => {
+            const getOfferedRes = await primarymanagerContract.getOffered(currencyOffered);
+            console.log("get offered response: ", getOfferedRes.response.result); //log response
+            (0, assert_1.default)(getOfferedRes.status === 0); //check no error(s)
+        });
+    });
+});
