54f136cbe87ac83322df81cbdc415706a6ee0be2
Mohzcrea8me <mohzcrea8me@gmail.com>
2024-05-22 23:45:47 +0800

    feat: Token contract update

--- dist/test/custodyGasslessTest.spec.js
+++ dist/test/custodyGasslessTest.spec.js
@@ -6,15 +6,18 @@ const index_1 = require("../wallet/index");
 const index_2 = require("../index");
 const ethers_1 = require("ethers");
 describe("Base Sepolia() Custody Gassless Tests", () => {
-    const CREATOR_PRIVATE_KEY = "0xf73343e93261bbe8189f798007ae731151f2d596f8b018f69ec9ebb1adc6197b";
+    // const CREATOR_PRIVATE_KEY =
+    //   "0xf73343e93261bbe8189f798007ae731151f2d596f8b018f69ec9ebb1adc6197b";
+    const newWallet = index_1.VerifiedWallet.createWallet();
+    console.log("new wallet: ", newWallet, Date.now().toString());
     const provider = new ethers_1.ethers.providers.JsonRpcProvider("https://sepolia.base.org");
-    const sender = new index_1.VerifiedWallet(CREATOR_PRIVATE_KEY, provider);
-    const signer = sender.setProvider(provider);
+    // const sender = new VerifiedWallet(CREATOR_PRIVATE_KEY, provider)
+    const signer = newWallet.setProvider(provider);
     describe("Tests some functions on custody contract", () => {
         const custodyAddress = "0xB1ae3Fc5B16d3736bf0db20606fB9a10b435392c";
         const custodyContract = new index_2.Custody(signer, custodyAddress);
         it("it should create vault", async () => {
-            const createVaultRes = await custodyContract.createVault("Tessst", "vltt");
+            const createVaultRes = await custodyContract.createVault(ethers_1.ethers.utils.formatBytes32String(Date.now().toString()), Date.now().toString());
             console.log("result: ", createVaultRes);
             (0, assert_1.default)(createVaultRes.status === 0);
         });
--- src/test/custodyGasslessTest2.spec.ts
+++ src/test/custodyGasslessTest2.spec.ts
@@ -4,12 +4,10 @@ import { VerifiedWallet } from "../wallet/index";
 import {Custody} from "../index";
 
 describe("Sepolia(11155111) Custody Gassless Tests", () => {      
-  const CREATOR_MNEMMONICS =
-    "omit floor sport lend knock alpha dumb three gentle hungry screen early";
   const INFURA_API_KEY =   "e0951a7b82c84a6880f3c6f7bb182e21"
-  const sender = VerifiedWallet.importWallet(CREATOR_MNEMMONICS);
+  const newWallet = VerifiedWallet.createWallet();
   const provider = Provider.infuraProvider(11155111, INFURA_API_KEY)
-  const signer = sender.setProvider(provider);
+  const signer = newWallet.setProvider(provider);
   describe("Tests some functions on custody contract", () => {
     const custodyAddress = "0x7aE9f79067AB4FDc8d41B18f1e6491590ac76f9d";
     const custodyContract = new Custody(
@@ -17,9 +15,9 @@ describe("Sepolia(11155111) Custody Gassless Tests", () => {
       custodyAddress
     );
     it("it should create vault", async () => {
-      const createVaultRes = await custodyContract.createVault("0x7665726966696564", "vltt");
-      console.log("result: ", createVaultRes)
+      const createVaultRes = await custodyContract.createVault(Date.now().toString(), Date.now().toString());
       assert(createVaultRes.response.status === 0);
+      console.log("result: ", createVaultRes)
     })
 
   });
