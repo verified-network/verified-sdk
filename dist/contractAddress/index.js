eacab7cecb70bd388c21e168b82ffa13bbd68df1
Kallol Borah <kallol.borah@gmail.com>
2025-04-01 01:12:04 +0530

    updated sdk 1.9.5 on sepolia for fix to custody contract

--- dist/contractAddress/index.js
+++ dist/contractAddress/index.js
@@ -328,7 +328,7 @@ const contractAddress = {
         'BalancerSecondaryIssueManager': '0xE3e3e9b8c0c292eD3756C7A1037322738de6B7FC',
         'MarginTradingPoolFactory': '0xB1ae3Fc5B16d3736bf0db20606fB9a10b435392c',
         'BalancerMarginIssueManager': '0xe1eA3F8c5951C6EDF5e892CCCa854e07d8ec8dA0',
-        'Custody': '0xb94029049E1bc514FE21516249d6f2c8D7B0488F',
+        'Custody': '0xA166F1f0dD40fFEBA529e327f17188d3Db0aB709',
         'Compound': '0x8c364b06741C9fE045E0e510A39BCbAf7445D8fD',
         'CASH': {
             'VCUSD': '0xC824FCCfe6545c42B609cF6aD6219748A306D83a',
