a7f4d99cbe4f04c85b8e0d6138a800ff7d77f79c
Mohzcrea8me <mohzcrea8me@gmail.com>
2025-04-15 15:51:04 +0100

    Delete dist directory

--- dist/models/bond/index.d.ts
+++
@@ -1,7 +0,0 @@
-interface RequestIssue {
-    _amount: number;
-    _payerAddress: string;
-    _currency: string;
-    _cashContractAddress: string;
-}
-export { RequestIssue };
--- dist/models/client/index.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-// SPDX-License-Identifier: BUSL-1.1
-Object.defineProperty(exports, "__esModule", { value: true });
